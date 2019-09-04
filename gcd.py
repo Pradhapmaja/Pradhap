c334c68fd87ae5af5935f4e72e69551da8327021
Pradhapmaja <54878871+Pradhapmaja@users.noreply.github.com>
2019-09-04 11:31:40 +0530

    Update gcd.py

--- gcd.py
+++ gcd.py
@@ -2,9 +2,10 @@ a,b = input().split()
 a = int(a)
 b = int(b)
 if(a>b):
-    for i in range (0,b+1):
-        if(a<b):
-            print(a)
-        else:
-            print(b)
-            break
+    small=b
+else:
+    small=a
+for i in range (1,small+1):
+    if(a%i==0)and(b%i==0):
+        gcd=i
+print(gcd)
