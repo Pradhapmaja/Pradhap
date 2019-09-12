df4d6b9f23ccc34638c110f533b8de7206e0c6f6
Pradhapmaja <54878871+Pradhapmaja@users.noreply.github.com>
2019-09-12 15:55:09 +0530

    Rename composite.py to compods.py

---
+++ compods.py
@@ -0,0 +1,15 @@
+def iscomposite(n):
+    if n<=1:
+        return False
+    if n<=3:
+        return False
+    if n%2==0 or n%3==0:
+        return True
+    i=5
+    while(i*i<=n):
+        if(n%i==0 or n%(i+2)==0):
+            return True
+        i+=6
+    return False
+n=int(input())
+print("yes") if(iscomposite(n)) else print("no")
